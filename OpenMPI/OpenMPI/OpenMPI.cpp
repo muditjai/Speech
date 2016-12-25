ea87a1660d820b751a19b2492bc2dbe882cbd6eb
Mudit Jain <muditjai@gmail.com>
2016-12-24 18:45:44 -0800

    Fix conflict

--- OpenMPI/OpenMPI/OpenMPI.cpp
+++ OpenMPI/OpenMPI/OpenMPI.cpp
@@ -4,12 +4,7 @@
  The master then merges to create a sorted array.
  *****/
 // Comment from a branch
-<<<<<<< HEAD
-// Comment merge from branch 2 and master
-=======
-// Another Comment from branch 2
-// Comment from branch 3
->>>>>>> tempBranch3
+// Comment merge from branch 2, 3 and master
 
 #include "stdafx.h"
 #include "mpi.h"
