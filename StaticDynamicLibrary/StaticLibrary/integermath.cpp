d7b266667a3578b9077991797d972bb0ea367c68
Mudit Jain <muditj@microsoft.com>
2017-07-19 11:34:40 -0700

    static lib

---
+++ StaticDynamicLibrary/StaticLibrary/integermath.cpp
@@ -0,0 +1,13 @@
+
+
+#include "integermath.h"
+
+int IntegerMath::divide(int a, int b)
+{
+	return a / b;
+}
+
+int IntegerMath::modulo(int a, int b)
+{
+	return a % b;
+}
---
+++ StaticDynamicLibrary/StaticLibrary/integermath.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class IntegerMath
+{
+public:
+	int divide(int a, int b);
+
+	int modulo(int a, int b);
+};
